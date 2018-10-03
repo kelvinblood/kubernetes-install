bf3b07cf84de26304a0ae9ced29d096c99cb9ea1
血衫非弧 <admin@kelu.org>
2018-10-03 08:56:25 +0800

    [feature] add a debian script

---
+++ install-debian.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [ `id -u` -ne 0 ];then
+   echo " [NOTE] root permit is needed!"
+   exit 1
+fi
+
+cdir="$(cd "$(dirname "$0")" && pwd)"
+
+curl -sSL https://get.docker.com/ | sh
+usermod -aG docker $USER
+# daemon.json
+mkdir -p /etc/docker
+if [ -f "/etc/docker/daemon.json" ];then
+    mv /etc/docker/daemon.json /etc/docker/daemon.json.bak.`date +%Y%m%d`
+fi
+sudo cp ${cdir}/cfg/daemon.json /etc/docker/
+
+# docker-compose
+if [ -f "/usr/bin/docker-compose" ];then
+    mv /usr/bin/docker-compose /usr/bin/docker-compose.bak.`date +%Y%m%d`
+fi
+chmod +x ${cdir}/cfg/docker-compose
+sudo cp ${cdir}/cfg/docker-compose /usr/bin/docker-compose
+
+# reload and start docker engine
+systemctl daemon-reload
+systemctl enable docker
+systemctl restart docker
+
