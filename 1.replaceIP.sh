a8105c0cba807c0908026adfc390618509e7be1f
血衫非弧 <admin@kelu.org>
2019-02-27 10:33:46 +0800

    [update]1.ip replace

--- 1.replaceIP.sh
+++ 1.replaceIP.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-sed -i "s/__ETCD_REPLACE_IP_1__/g" `ls -R`
-sed -i "s/__ETCD_REPLACE_IP_2__/g" `ls -R`
-sed -i "s/__ETCD_REPLACE_IP_3__/g" `ls -R`
+sed -i "s/__ETCD_REPLACE_IP_1__/g" `ls`
+sed -i "s/__ETCD_REPLACE_IP_2__/g" `ls`
+sed -i "s/__ETCD_REPLACE_IP_3__/g" `ls`
 
+cd cfg
+
+sed -i "s/__ETCD_REPLACE_IP_1__/g" `ls`
+sed -i "s/__ETCD_REPLACE_IP_2__/g" `ls`
+sed -i "s/__ETCD_REPLACE_IP_3__/g" `ls`
 
 
