0da39f1f401a6f830efeb51f9ed2df03a676ff9e
血衫非弧 <admin@kelu.org>
2019-02-27 10:22:32 +0800

    [init] etcd ip replace

--- 4.check.sh
+++ 4.check.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 etcdctl \
-  --ca-file=/app/allblue/etcd/ssl/ca.pem \
-  --cert-file=/app/allblue/etcd/ssl/peer.pem \
-  --key-file=/app/allblue/etcd/ssl/peer-key.pem \
-  --endpoints=https://10.19.0.70:2379,https://10.19.0.71:2379,https://10.19.0.72:2379  \
+  --ca-file=/app/kelu/etcd/ssl/ca.pem \
+  --cert-file=/app/kelu/etcd/ssl/peer.pem \
+  --key-file=/app/kelu/etcd/ssl/peer-key.pem \
+  --endpoints=https://__ETCD_REPLACE_IP_1__:2379,https://__ETCD_REPLACE_IP_2__:2379,https://__ETCD_REPLACE_IP_3__:2379  \
   cluster-health
