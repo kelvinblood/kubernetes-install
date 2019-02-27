8a8c765e56e215be9f175baaeee67ae8ed5bca71
血衫非弧 <admin@kelu.org>
2019-02-27 10:28:17 +0800

    [update] etcd

--- 1.replaceIP.sh
+++ 1.replaceIP.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
-sed -i "s/__ETCD_REPLACE_IP_1__/g" generateCA.sh
-sed -i "s/__ETCD_REPLACE_IP_2__/g" generateCA.sh
-sed -i "s/__ETCD_REPLACE_IP_3__/g" generateCA.sh
-
-sed -i "s/__ETCD_REPLACE_IP_1__/g" check.sh
-sed -i "s/__ETCD_REPLACE_IP_2__/g" check.sh
-sed -i "s/__ETCD_REPLACE_IP_3__/g" check.sh
-
-cd cfg
-
-sed -i "s/__ETCD_REPLACE_IP_1__/g" `ls`
-sed -i "s/__ETCD_REPLACE_IP_2__/g" `ls`
-sed -i "s/__ETCD_REPLACE_IP_3__/g" `ls`
+sed -i "s/__ETCD_REPLACE_IP_1__/g" `ls -R`
+sed -i "s/__ETCD_REPLACE_IP_2__/g" `ls -R`
+sed -i "s/__ETCD_REPLACE_IP_3__/g" `ls -R`
 
 
 
